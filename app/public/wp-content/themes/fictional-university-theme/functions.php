2e343af9e6afc9e756c6dda6d67e7fa1ba9f79af
unknown <tpham20908@gmail.com>
2018-01-27 15:37:48 -0500

    get html, css, images file from fictional university project

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,4 +1,11 @@
-<?php function university_files() {
-  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+<?php function college_files() {
+  // link to external customed font
+  wp_enqueue_style('custom_google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  // link to external font awesome
+  wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  // link to internal style.css
+  wp_enqueue_style('college_main_styles', get_stylesheet_uri());
 } ?>
-<?php add_action('wp_enqueue_scripts', 'university_files'); ?>
+
+<!-- assign custom function name college_files -->
+<?php add_action('wp_enqueue_scripts', 'college_files'); ?>
