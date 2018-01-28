4b2b08a9ce4eda7246a633bec28563ff2c66d4a3
unknown <tpham20908@gmail.com>
2018-01-28 18:30:17 -0500

    Remove the subtitle underneath the title temporarily

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -8,7 +8,7 @@
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title(); ?></h1>
       <div class="page-banner__intro">
-        <p>Learn how the school of your dreams got started.</p>
+        <p>Don't forget to place me later.</p>
       </div>
     </div>
   </div>
