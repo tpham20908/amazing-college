52668f72557ef67953f88cea67073676debc23a7
unknown <tpham20908@gmail.com>
2018-01-27 22:33:30 -0500

    call microtime() in wp_enqueue_style func to avoid caching css

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -4,7 +4,7 @@
   // link to external font awesome
   wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   // link to internal style.css
-  wp_enqueue_style('college_main_styles', get_stylesheet_uri());
+  wp_enqueue_style('college_main_styles', get_stylesheet_uri(), null, microtime());
   // link to internal javascript file
   // wp_enqueue_script(name, get_theme_file_uri(), dependancy, version, place it before body closing);
   wp_enqueue_script('college_main_js', get_theme_file_uri('js/scripts-bundled.js'), null, microtime(), true);
