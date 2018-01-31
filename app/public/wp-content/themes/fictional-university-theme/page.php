b97950ff5cf7b7492d3a33df5301dcdc152c22ad
unknown <tpham20908@gmail.com>
2018-01-30 21:09:00 -0500

    create children links in page-links section

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -16,8 +16,8 @@
   <div class="container container--narrow page-section">
 
   <?php
-  $theParent = wp_get_post_parent_ID(get_the_ID());
-  if ($theParent) { ?>
+    $theParent = wp_get_post_parent_ID(get_the_ID());
+    if ($theParent) { ?>
 
     <div class="metabox metabox--position-up metabox--with-home-link">
       <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
@@ -25,15 +25,25 @@
 
   <?php } ?>
 
-<!--
     <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php
+          if ($theParent) {
+            $findChildrenOf = $theParent;
+          } else {
+            $findChildrenOf = get_the_ID();
+          }
+
+          wp_list_pages(array(
+            'title_li' => null,
+            'child_of' => $findChildrenOf
+          ));
+
+        ?>
       </ul>
     </div>
- -->
+
     <div class="generic-content">
       <?php the_content(); ?>
     </div>
