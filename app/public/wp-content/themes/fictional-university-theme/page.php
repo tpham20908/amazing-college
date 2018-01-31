b6ad3121849b1b39a9123bb90d97c56512884c4d
unknown <tpham20908@gmail.com>
2018-01-30 21:31:57 -0500

    style page-links section for pages which do not have children nor parent pages

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -25,6 +25,11 @@
 
   <?php } ?>
 
+    <?php
+    $testArray = get_pages(array(
+      'child_of' => get_the_ID()
+    ));
+    if ($theParent or $testArray) { ?>
     <div class="page-links">
       <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent) ?></a></h2>
       <ul class="min-list">
@@ -43,6 +48,7 @@
         ?>
       </ul>
     </div>
+    <?php } ?>
 
     <div class="generic-content">
       <?php the_content(); ?>
